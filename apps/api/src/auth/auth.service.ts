d1b00777dec9971a93b9f7d49c5de0d7f5cfd362
Bruno Hirae <brunodeluca117@outlook.com>
2025-05-12 14:29:55 -0300

    feat(user): implement UserModule, UserController, and UserService; add CreateUserDto and UpdateUserDto

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup(createUserDto: CreateUserDto) {
+    console.log('Creating user:', createUserDto);
+    throw new Error('Method not implemented.');
+  }
+}
