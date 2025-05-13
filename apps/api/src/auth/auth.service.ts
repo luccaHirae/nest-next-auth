5c81619c6bc8d794030ee19ed7378387c91218d0
Bruno Hirae <brunodeluca117@outlook.com>
2025-05-13 13:29:25 -0300

    feat(auth): add local authentication strategy with validation and error handling

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { verify } from 'argon2';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
 
@@ -12,5 +17,21 @@ export class AuthService {
     if (user) throw new ConflictException('User already exists');
 
     return this.userService.create(createUserDto);
+  }
+
+  async validateLocalUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) throw new UnauthorizedException('User not found');
+
+    const isPasswordValid = await verify(user.password, password);
+
+    if (!isPasswordValid)
+      throw new UnauthorizedException('Invalid credentials');
+
+    return {
+      id: user.id,
+      name: user.name,
+    };
   }
 }
