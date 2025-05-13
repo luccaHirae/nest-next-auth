00f9862b6ecdda6cc7e68527fc1ee3f028812e32
Bruno Hirae <brunodeluca117@outlook.com>
2025-05-13 14:34:17 -0300

    feat(auth): add AppBar and SignInButton components for user navigation and authentication

---
+++ apps/web/components/appbar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import { SignInButton } from '@/components/signin-button';
+
+export function AppBar() {
+  return (
+    <div className='p-2 shadow flex gap-3 bg-gradient-to-br from-blue-400 to-cyan-400 text-white'>
+      <Link href='/'>Home</Link>
+      <Link href='/dashboard'>Dashboard</Link>
+      <SignInButton />
+    </div>
+  );
+}
