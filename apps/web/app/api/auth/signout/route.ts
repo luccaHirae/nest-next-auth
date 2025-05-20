dd5ca4e9844b9de1aa030ee29cecacef2cae3ba9
Bruno Hirae <brunodeluca117@outlook.com>
2025-05-20 13:28:28 -0300

    feat(auth): update signout route to fetch from backend and handle session deletion

--- apps/web/app/api/auth/signout/route.ts
+++ apps/web/app/api/auth/signout/route.ts
@@ -1,9 +1,18 @@
+import { BACKEND_URL } from '@/constants';
+import { authFetch } from '@/lib/auth-fetch';
 import { deleteSession } from '@/lib/session';
 import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
-  await deleteSession();
-  revalidatePath('/');
+  const response = await authFetch(`${BACKEND_URL}/auth/signout`, {
+    method: 'POST',
+  });
+
+  if (response.ok) await deleteSession();
+
+  revalidatePath('/', 'layout');
+  revalidatePath('/', 'page');
+
   return NextResponse.redirect(new URL('/', req.nextUrl));
 }
